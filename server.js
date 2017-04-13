71ab0b692d98df1fb71546f563af9b6f0c596af5
Markus Latvala <markus.latvala@opuscapita.com>
2017-04-13 14:13:02 +0300

    First functional version

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ async function Startup() {
     const app = express(); // Our application singleton
 
     // Authentication
-    const session = require('express-session');
     const passport = require('passport');
     const bodyParser = require('body-parser');
+    const cookieParser = require('cookie-parser');
+    const cookieSession = require('cookie-session');
     const Issuer = require('openid-client').Issuer;
     const Strategy = require('openid-client').Strategy;
 
@@ -41,35 +42,28 @@ async function Startup() {
         redirect_uri: 'http://localhost:5000/node/signin-oidc',
         response_type: 'code id_token',
         response_mode: 'form_post',
-        acr_values: 'tenant:default clienthost:http://localhost:5000 idp:opuscapita',
+        acr_values: 'tenant:default clienthost:http://localhost:5000',// idp:opuscapita',
     }
 
     // Passport user serialization
     passport.serializeUser(function (user, done) {
-        console.log("serializing " + user.id);
         done(null, user);
     });
 
-    passport.deserializeUser(function (id, done) {
-        console.log("deserializing " + id);
-
-        let user = {
-            id: id
-        };
-
-        done(null, user);
+    passport.deserializeUser(function (obj, done) {
+        done(null, obj);
     });
 
     // Initialize passport strategy
     passport.use('oidc',
         new Strategy({ client, params }, (tokenset, done) => {
-            console.log('tokenset', tokenset);
-            console.log('access_token', tokenset.access_token);
-            console.log('id_token', tokenset.id_token);
-            console.log('claims', tokenset.claims);
+            console.log('User ' + tokenset.claims.sub + ' successfully logged in');
 
             let user = {
-                id: tokenset.claims.sub
+                id: tokenset.claims.sub,
+                name: tokenset.claims.name,
+                id_token: tokenset.id_token,
+                claims: tokenset.claims
             };
 
             return done(null, user);
@@ -79,11 +73,29 @@ async function Startup() {
     // View engine setup
     app.set(express.static(__dirname + '/public'));
 
-    // Middleware
-    app.use(session({ secret: 'Ceiling cat', resave: true, saveUninitialized: true }));
+    // Needed for parsing form type responses
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+
+    // Needed for cookie session storage
+    app.use(cookieParser(
+        'cookie-secret'
+    ));
+
+    // Store session in a cookie
+    app.use(cookieSession({
+        name: 'opuscapita-auth',
+        secret: 'cookie-secret'
+    }));                    
+
     app.use(passport.initialize());
     app.use(passport.session());
-    app.use(bodyParser.urlencoded({ extended: false }));
+
+    // Login failed page
+    app.get('/loginfailed', function (request, response) {
+        response.send('<html><body><p>Oh no. Looks like you failed to log in</p></body></html>');
+    });
 
     // Login
     app.get('/node/login', passport.authenticate('oidc'));
@@ -92,36 +104,51 @@ async function Startup() {
     app.post('/node/signin-oidc', function (req, res, next) {
         passport.authenticate('oidc', function (err, user, info) {
 
+            console.log('Authorize endpoint response received');
+
             // Error!
             if (err) {
+                console.log('Authentication failed');
                 console.log(err);
                 return next(err); // will generate a 500 error
             }
 
             // Generate a JSON response reflecting authentication status
             if (!user) {
-                return res.send(401, { success: false, message: 'authentication failed' });
+                console.log('Authentication failed. The response did not contain an identity');
+                return res.redirect('/loginfailed');
             }
 
             // Login the user
             req.login(user, function (err) {
                 if (err) {
+                    console.log('Login failed');
+                    console.log(err);
                     return next(err);
                 }
 
-                return res.send(200, { success: true, message: 'successfully authenticated', user: user });
-                //return res.redirect('/node');
+                return res.redirect('/node');
             });
         })(req, res, next);
     });
 
-    // Root
-    app.get('/node', passport.authenticate('oidc'), function (request, response) {
-        response.send('<html><body><p>Node.js says hello!</p></body></html>');
+    // Passport will set the request.user when authenticated
+    function isLoggedIn (request, response, next) {
+        if (request.user) {
+            next();
+        } else {
+            response.redirect('/login');
+        }
+    };
+
+    // Success
+    app.get('/node', isLoggedIn, function (request, response) {
+        response.send('<html><body><p>Hello there, ' + request.user.name + '!</p></body></html>');
     });
 
+    // Start server
     var server = http.createServer(app);
     server.listen(5000);
 
-    console.log('Server listening to port 5000...');
+    console.log('Server started and listening to port 5000...');
 }
